64b0ac786c02f96edd59475aa9f533368c201883
adityad85 <aditya85d@gmail.com>
2018-06-19 23:41:21 +0530

    WIP

--- src/components/02-MovieList.js
+++ src/components/02-MovieList.js
@@ -12,8 +12,10 @@ class MovieList extends Component {
   // Refer to App.css for a list a classes or add one yourself
   render() {
     return (
-      <ul className="FILL ME">
+      <ul className="movies-list">
         <li>The Godfather</li>
+        <li>Interstellar</li>
+        <li>Arrival</li>
       </ul>
     );
   }
