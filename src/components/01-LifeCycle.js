73ec36d864c3a4d2486bdfcb29614b806d9db38c
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-26 04:07:06 +0530

    Finished 01-lifecycle

--- src/components/01-LifeCycle.js
+++ src/components/01-LifeCycle.js
@@ -1,3 +1,5 @@
+/* eslint-disable-no-console */
+
 import React from 'react';
 
 /*
@@ -12,6 +14,7 @@ class LifeCycle extends React.Component {
   constructor(props) {
     super(props);
     this.state = { name: 'Pesto' };
+    this.onClick = this.onClick.bind(this);
   }
 
   // This code will be called when the component finishes mounting
@@ -20,25 +23,36 @@ class LifeCycle extends React.Component {
     // Task 1: Display a message "I'm mounted!" in developer's console when the
     //         component finishes mounting.
     //         Use `console.log` function for it.
+    console.log("I'm mounted!");
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate() {
     // Task 2: Display a message "Updated!" in developer's console
     //         when the component updates.
     //         Here you also need to use the console.log function.
     //         Notice that in this lifecycle method you have an access
     //         to previous values of properties and state.
-    console.log({ prevProps, prevState });
+    console.log('Updated!');
   }
 
   componentWillUnmount() {
     // Task 3: Display a message "Good night, Pesto!" in developer's
     //         console when the component unmounts.
+    console.log('Good night, Pesto!');
+  }
+
+  onClick() {
+    this.setState({ name: 'Pesto Tech' });
   }
 
   // Task 4: Create a button which triggers ComponentDidUpdate lifecycle method on click.
   render() {
-    return (<p>Good morning, {this.state.name}!</p>);
+    return (
+      <div>
+        <button onClick={this.onClick}>Click me to update!</button>
+        <p>Good morning, {this.state.name}!</p>
+      </div>
+    );
   }
 }
 
